931aeee06fd6f32b2fef2384e28cdb2a7c68bdb5
Sergey <serg80997@mail.ru>
2020-10-19 17:05:14 +0300

    Жизненные циклы изменения, удаления

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import Car from './Car/Car'
 
 class App extends Component {
 
-  state = {
-    cars: [
-      {name: 'Ford', year: 2018},
-      {name: 'Audi', year: 2016},
-      {name: 'Mazda', year: 2010}
-    ],
-    pageTitle: 'React components',
-    showCars: false
+  constructor(props) {
+    console.log('App constructor')
+    super(props)
+
+    this.state = {
+      cars: [
+        {name: 'Ford', year: 2018},
+        // {name: 'Audi', year: 2016},
+        // {name: 'Mazda', year: 2010}
+      ],
+      pageTitle: 'React components',
+      showCars: false
+    }
   }
 
   toggleCarsHandler = () => {
@@ -36,7 +41,16 @@ class App extends Component {
 
   }
 
+  componentWillMount() {
+    console.log('App componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('App componentDidMount')
+  }
+
   render() {
+    console.log('App render')
     const divStyle = {
       textAlign: 'center'
     }
@@ -59,13 +73,20 @@ class App extends Component {
 
     return (
       <div style={divStyle}>
-        <h1>{this.state.pageTitle}</h1>
+        {/*<h1>{this.state.pageTitle}</h1>*/}
+        <h1>{this.props.title}</h1>
 
         <button
           onClick={this.toggleCarsHandler}
         >Toggle cars</button>
 
-        { cars }
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: '20px'
+        }}>
+          { cars }
+        </div>
       </div>
     );
   }
